041040b2da301ba8948320410a272163455ac7d1
Viet Do <vietdonguyen@hotmail.de>
2020-06-01 16:57:29 +0200

    add routes =, include bodyparser for json

--- rest/index.js
+++ rest/index.js
@@ -1,13 +1,22 @@
 const mongoose = require('mongoose');
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-mongoose.connect('mongodb://mongo/node-ms-rest', {useNewUrlParser: true, useUnifiedTopology: true});
+const chefRoutes = require('./routes/chef.routes');
+const dishRoutes = require('./routes/dish.routes');
+
+mongoose.connect('mongodb://mongo/node-ms-rest', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 
 mongoose.connection.once('open', () => {
   console.log('connected to database');
 })
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/chef', chefRoutes);
+app.use('/dish', dishRoutes);
 app.listen(3000, () => {
   console.log('Server running on port 3000')
 })
