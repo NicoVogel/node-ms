042f67b2b2080722324e4b9191b310297294048b
NicoVogel <nico-vogel97@outlook.de>
2020-06-04 01:54:05 +0200

    add live reload

--- lan/app/nginx/docker_entrypoint.sh
+++ lan/app/nginx/docker_entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
-envsubst '${PORT},${SERVER_NAME},${EXPIRES}' < /scripts/nginx.conf.template > /etc/nginx/nginx.conf
+if [[ -z "${DEPLOY_ENV}" ]]; then
+  export ANGULAR_FORWARD=`location /{
+            proxy_pass         http://localhost:4200;
+            proxy_redirect     off;
+            proxy_set_header   Host $host;
+        }`
+else
+  export ANGULAR_LOCAL=`root /app;`
+fi
+
+
+
+
+envsubst '${PORT},${SERVER_NAME},${ANGULAR_FORWARD},${ANGULAR_LOCAL}' < /scripts/nginx.conf.template > /etc/nginx/nginx.conf
 
 nginx -g "daemon off;"
