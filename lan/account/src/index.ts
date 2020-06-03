9d9b93bed27595c87ca2ecb87f080ef23a0c1c6b
Viet Do <vietdonguyen@hotmail.de>
2020-06-04 00:54:33 +0200

    add mongoose

--- lan/account/src/index.ts
+++ lan/account/src/index.ts
@@ -1,7 +1,12 @@
 import * as express from 'express';
+import * as mongoose from 'mongoose';
 
 const app = express();
 
+mongoose.connect('mongodb://mongodb:27017/node-ms-account', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+mongoose.connection.once('open', () => {
+  console.log('Service Account connected to database');
+})
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
 );
