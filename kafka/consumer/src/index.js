28ccc842fe51a6bdf098f6f83fdce7f3ddfa7445
NicoVogel <nico-vogel97@outlook.de>
2020-06-03 06:25:43 +0000

    works

--- kafka/consumer/src/index.js
+++ kafka/consumer/src/index.js
@@ -1,8 +1,49 @@
-var kafka = require('kafka-node');
-var Consumer = kafka.Consumer,
-    client = new kafka.KafkaClient({kafkaHost: 'kafka:9092'}),
-    consumer = new Consumer(
-        client, [{ topic: 'test1', partition: 0 }], { autoCommit: false });
-consumer.on('message', function (message) {
-    console.log(message);
-});
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'consumer',
+    brokers: ['kafka:9092']
+})
+
+const topic = 'logs'
+const consumer = kafka.consumer({ groupId: 'test-group' })
+
+const run = async () => {
+    await consumer.connect()
+    await consumer.subscribe({ topic, fromBeginning: true })
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`
+            console.log(`- ${prefix} ${message.key}#${message.value}`)
+        },
+    })
+}
+run().catch(e => console.error(`[example/consumer] ${e.message}`, e))
+
+
+
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.map(type => {
+    process.on(type, async e => {
+        try {
+            console.log(`process.on ${type}`)
+            console.error(e)
+            await consumer.disconnect()
+            process.exit(0)
+        } catch (_) {
+            process.exit(1)
+        }
+    })
+})
+
+signalTraps.map(type => {
+    process.once(type, async () => {
+        try {
+            await consumer.disconnect()
+        } finally {
+            process.kill(process.pid, type)
+        }
+    })
+})
--- kafka/producer/src/index.js
+++ kafka/producer/src/index.js
@@ -1,8 +1,49 @@
-var kafka = require('kafka-node');
-var Consumer = kafka.Consumer,
-    client = new kafka.KafkaClient({kafkaHost: 'kafka:9092'}),
-    consumer = new Consumer(
-        client, [{ topic: 'test1', partition: 0 }], { autoCommit: false });
-consumer.on('message', function (message) {
-    console.log(message);
-});
+const { Kafka } = require('kafkajs')
+
+const kafka = new Kafka({
+    clientId: 'producer',
+    brokers: ['kafka:9092']
+})
+
+const producer = kafka.producer();
+
+const run = async () => {
+    await producer.connect()
+    await producer.send({
+        topic: 'logs',
+        messages: [
+          { value: 'Hello KafkaJS user!' },
+        ],
+      })
+    console.log('send message')
+}
+
+run().catch(e => console.error(`[example/producer] ${e.message}`, e))
+
+
+
+
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.map(type => {
+    process.on(type, async () => {
+        try {
+            console.log(`process.on ${type}`)
+            await producer.disconnect()
+            process.exit(0)
+        } catch (_) {
+            process.exit(1)
+        }
+    })
+})
+
+signalTraps.map(type => {
+    process.once(type, async () => {
+        try {
+            await producer.disconnect()
+        } finally {
+            process.kill(process.pid, type)
+        }
+    })
+})
