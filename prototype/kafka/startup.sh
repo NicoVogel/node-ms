61b8a3a668dc98ecb407ec4ec976da4c839cb20d
Nico Vogel <nico-vogel97@outlook.de>
2020-06-04 22:41:00 +0200

    create .env file automatically

--- prototype/kafka/startup.sh
+++ prototype/kafka/startup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "KAFKA_ADVERTISED_HOST_NAME=$(dig +short myip.opendns.com @resolver1.opendns.com)" > .env & wait
+echo "created .env file" 
+cat .env
+
 # clear first
 docker-compose down -v
 
