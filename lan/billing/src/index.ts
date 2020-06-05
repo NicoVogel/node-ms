ada42937b808d0d33f78eaed5cf4c3a312fa8235
Nguyen Viet Do <vietdonguyen@hotmail.de>
2020-06-05 18:54:57 +0000

    add express index

--- lan/billing/src/index.ts
+++ lan/billing/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import * as express from 'express';
+import * as cors from 'cors';
+import * as bodyParser from 'body-parser';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(3000, () =>
+  console.log('Example app listening on port 3000!'),
+);
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
