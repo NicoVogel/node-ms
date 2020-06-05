982c21889c124d013262789906131b10a2af8812
Nguyen Viet Do <vietdonguyen@hotmail.de>
2020-06-05 19:44:22 +0000

    add requestPayment

--- lan/billing/src/controller/payment.controller.ts
+++ lan/billing/src/controller/payment.controller.ts
@@ -1,1 +1,20 @@
 import { Model } from 'mongoose';
+import { Request, NextFunction, Response } from 'express';
+import * as db from '../models';
+const Payment = db.default.Payment;
+
+export function requestPayment(req: Request, res: Response) {
+  if (!req.body.userId) {
+    res.status(400).send({ message: "Content can not be empty" });
+  }
+  const payment = new Payment(req.body);
+
+  payment.save()
+    .then(data => res.send(data.id))
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || 'Error',
+      });
+    })
+}
