1a548f36be9de0c198b44ac6417f5a7bcc409434
Nguyen Viet Do <vietdonguyen@hotmail.de>
2020-06-05 13:58:39 +0000

    add debug view

--- lan/app/src/app/about/about.component.ts
+++ lan/app/src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) { }
 
   ngOnInit(): void {
   }
 
+  debugUser(): string {
+    return JSON.stringify(this.authenticationService.currentUserValue, null, 4);
+  }
 }
