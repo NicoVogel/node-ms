35ea1bb9ea777b11fd424622f0f49a7955b151d2
Nguyen Viet Do <vietdonguyen@hotmail.de>
2020-06-05 23:18:04 +0000

    add functions

--- lan/billing/src/controller/payment.controller.ts
+++ lan/billing/src/controller/payment.controller.ts
@@ -1,24 +1,89 @@
-import { Model } from 'mongoose';
-import { Request, NextFunction, Response } from 'express';
+import { Request, Response } from 'express';
 import * as db from '../models';
+import { ICart } from '../models/payment.model';
 const Payment = db.default.Payment;
 
-// export function requestPayment(req: Request, res: Response) {
-//   if (!req.body.id || !req.body.id.accountId || !req.body.id.eventId) {
-//     res.status(400).send({ message: "Content can not be empty" });
-//     return;
-//   }
-//   const payment = new Payment(req.body);
+export async function requestPayment(paymentObj: Object) {
+  const payment = new Payment(paymentObj);
+  // ignore incoming state
+  payment.state = 'REQUESTED';
+  payment.save()
+    .then(data => console.log(data))
+    .catch(err => {
+      console.error(err);
+    })
+}
 
-//   payment.save()
-//     .then(data => res.send(data.id))
-//     .catch(err => {
-//       res.status(500).send({
-//         message:
-//           err.message || 'Error in requesting Payment',
-//       });
-//     })
-// }
+export async function pendingPayment(_id: Object) {
+  setState(_id, "PENDING");
+}
+
+export async function completedPayment(_id: Object) {
+  setState(_id, "COMPLETED");
+}
+
+export async function setState(_id: Object, state: string) {
+  const target = await Payment.findOne({ _id });
+  if (!target) {
+    console.warn("account-event pair does not exist")
+    return;
+  }
+  target.transactionDate = new Date();
+  target.state = state;
+  target.save();
+}
+
+export async function addCartElementArray(_id: Object, cartArray: ICart[], replace = false) {
+  const target = await Payment.findOne({ _id });
+
+  if (!target) {
+    console.warn("account-event pair does not exist")
+    return;
+  }
+  if (replace) {
+    target.cart = cartArray;
+  } else {
+    target.cart.push(...cartArray);
+  }
+  target.save();
+}
+
+export async function removeCartElement(_id: Object, sourceId: string, purpose: string) {
+  const target = await Payment.findOne({ _id });
+  if (!target) {
+    console.error("account-event pair does not exist")
+    return;
+  } else {
+    const index = target.cart.findIndex(cartElem => cartElem.sourceId === sourceId && cartElem.purpose === purpose);
+    if (index >= 0) {
+      target.cart = [...target.cart.slice(0, index), ...target.cart.slice(index + 1)];
+      target.save();
+    } else console.error("cart with sourceid-purpose pair does not exist")
+  }
+}
+
+// REST
+
+export function test(req: Request, res: Response) {
+  // Payment.collection.drop();
+  // addCartElementArray({ accountId: 'account5', eventId: 'event4' }, [{
+  //   "sourceId": "aaaa",
+  //   "purpose": "purp12",
+  //   "amount": 52.5
+  // }], true)
+  // completedPayment({ accountId: 'account5', eventId: 'event4' });
+  removeCartElement({ accountId: 'account5', eventId: 'event4' }, "aaaa", "purp12");
+  res.end();
+}
+
+export function add(req: Request, res: Response) {
+  if (!req.body._id || !req.body._id.accountId || !req.body._id.eventId) {
+    res.status(400).send({ message: "Content can not be empty" });
+    return;
+  }
+  requestPayment(req.body);
+  res.end();
+}
 
 export function getAll(req: Request, res: Response) {
   Payment.find()
--- lan/billing/src/routes/payment.routes.ts
+++ lan/billing/src/routes/payment.routes.ts
@@ -2,8 +2,9 @@ import * as express from 'express';
 import * as PaymentController from '../controller/payment.controller'
 const PaymentRouter = express.Router();
 
-// PaymentRouter.post('/request', PaymentController.requestPayment);
+PaymentRouter.post('/request', PaymentController.add);
 PaymentRouter.get('/:accountId/:eventId', PaymentController.getById);
 // PaymentRouter.put('/:id', PaymentController.update);
 PaymentRouter.get('/', PaymentController.getAll);
+PaymentRouter.get('/test', PaymentController.test);
 export default PaymentRouter;
