838dd45521599910b2de47ea496d8dca74232f17
Nguyen Viet Do <vietdonguyen@hotmail.de>
2020-06-05 12:47:04 +0000

    add logout for navbar

--- lan/app/src/app/app.component.ts
+++ lan/app/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from './models/User';
+import { Router } from '@angular/router';
+import { AuthenticationService } from './authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +11,14 @@ import { User } from './models/User';
 export class AppComponent {
   active = -1;
   currentUser: User;
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+  }
 }
