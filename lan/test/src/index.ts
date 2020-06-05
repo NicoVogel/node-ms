1c1332a0c89bce902e28c4f4e6bcda5764c9035f
Nico Vogel <nico-vogel97@outlook.de>
2020-06-05 23:04:19 +0000

    use the new adapter

--- lan/test/src/index.ts
+++ lan/test/src/index.ts
@@ -1,36 +1,7 @@
-import * as Amqp from 'amqp-ts';
+import { EventAdapter } from './services/rabbitmq.service';
 
-const connection = new Amqp.Connection('amqp://rabbitmq');
+const eventAdapter = new EventAdapter();
+eventAdapter.subscribe('account.created').subscribe(console.log);
+eventAdapter.active();
 
-// definitions
-const accountExchange = connection.declareExchange('account', 'topic', {
-  durable: false
-});
-const q1 = connection.declareQueue('q1', { durable: false });
-const q2 = connection.declareQueue('q2', { durable: false });
-const q3 = connection.declareQueue('q3', { durable: false });
-
-// bindungs
-q1.bind(accountExchange, 'account.created');
-q2.bind(accountExchange, 'account.created');
-q3.bind(accountExchange, 'account.register');
-
-
-q1.activateConsumer((message) => {
-  console.log('Message received q1: ' + message.getContent());
-});
-q2.activateConsumer((message) => {
-  console.log('Message received q2: ' + message.getContent());
-});
-q3.activateConsumer((message) => {
-  console.log('Message received q3: ' + message.getContent());
-  message.ack();
-});
-
-
-connection.completeConfiguration().then(() => {
-  // the following message will be received because
-  // everything you defined earlier for this connection now exists
-  const msg2 = new Amqp.Message('hi');
-  // accountExchange.send(msg2, 'account.register');
-});
+eventAdapter.publish('account.created', "hi")
