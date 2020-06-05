7917c37c06c681f03559190d08189809571b38cc
Nico Vogel <nico-vogel97@outlook.de>
2020-06-05 21:25:41 +0000

    test multiple receivers

--- lan/test/src/index.ts
+++ lan/test/src/index.ts
@@ -6,20 +6,30 @@ const connection = new Amqp.Connection('amqp://rabbitmq');
 const accountExchange = connection.declareExchange('account', 'topic', {
   durable: false
 });
-const queue = connection.declareQueue('');
+const q1 = connection.declareQueue('q1', { durable: false });
+const q2 = connection.declareQueue('q2', { durable: false });
+const q3 = connection.declareQueue('q3', { durable: false });
 
 // bindungs
-queue.bind(accountExchange, 'account.created');
+q1.bind(accountExchange, 'account.created');
+q2.bind(accountExchange, 'account.created');
+q3.bind(accountExchange, 'account.register');
 
 
-queue.activateConsumer((message) => {
-  console.log('Message received: ' + message.getContent());
+q1.activateConsumer((message) => {
+  console.log('Message received q1: ' + message.getContent());
+});
+q2.activateConsumer((message) => {
+  console.log('Message received q2: ' + message.getContent());
+});
+q3.activateConsumer((message) => {
+  console.log('Message received q3: ' + message.getContent());
 });
 
 
 connection.completeConfiguration().then(() => {
   // the following message will be received because
   // everything you defined earlier for this connection now exists
-  const msg2 = new Amqp.Message('Test2');
-  accountExchange.send(msg2, 'account.created');
+  const msg2 = new Amqp.Message('hi');
+  accountExchange.send(msg2, 'account.register');
 });
