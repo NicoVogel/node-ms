8d1d8c71d6363b358f46ca3909db6546ace32a76
Nguyen Viet Do <vietdonguyen@hotmail.de>
2020-06-06 14:12:54 +0000

    prettify check account

--- lan/billing/src/controller/ext.account.controller.ts
+++ lan/billing/src/controller/ext.account.controller.ts
@@ -1,18 +1,18 @@
 import * as db from '../models';
+import { Types } from 'mongoose';
 const Account = db.default.Account;
 
 export async function addAccount(accountObj: Object) {
   const account = new Account(accountObj);
 
-  account.save().catch(err => {
-    console.error(err);
-  });
+  account.save()
+    // .then(data => console.log("account saved with id" + data.id))
+    .catch(err => {
+      console.error(err);
+    });
 }
 
-export async function checkAccount(accountId: string): Promise<Boolean> {
-  const target = await Account.findById(accountId);
-  if (target) {
-    return true;
-  }
-  return false;
+export async function checkAccount(accountId: string): Promise<boolean> {
+  return Types.ObjectId.isValid(accountId) &&
+    await Account.findById(accountId) !== null;
 }
