aa47516d5ee74601b71545648dfdb41def432a23
Nguyen Viet Do <vietdonguyen@hotmail.de>
2020-06-06 14:49:48 +0000

    adjust initamqp and flatten topickey config

--- lan/billing/src/services/index.ts
+++ lan/billing/src/services/index.ts
@@ -4,19 +4,16 @@ import { addAccount } from '../controller/ext.account.controller';
 export const eventAdapter = new EventAdapter();
 
 export function initAMQP() {
-  for (const service in topicKeys) {
-    if (topicKeys.hasOwnProperty(service)) {
-      const serviceEvent: string[] = topicKeys[service];
+  // topicKeys.forEach(key => {
+  //   try {
+  //     eventAdapter.listen(key).subscribe(e => console.log(`${key}: \t\t${JSON.stringify(e)} `));
+  //   } catch (error) {
+  //     console.error(`wrong topicKey in config! the wrong key is: ${key}. Error: ${error.getMessage()} `);
+  //   }
+  // })
 
-      // serviceEvent.forEach(event => {
-      //   eventAdapter.listen(event).subscribe(e => console.log(`${event}:\t\t${JSON.stringify(e)}`));
-      // });
-
-      if (serviceEvent.includes('account.created')) {
-        eventAdapter.listen('account.created').subscribe(data => addAccount(data));
-      }
-
-    }
+  if (topicKeys.includes('account.created')) {
+    eventAdapter.listen('account.created').subscribe(data => addAccount(data));
   }
   eventAdapter.activate();
 }
