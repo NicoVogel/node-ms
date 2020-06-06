23deef066d9433f55c8bccd8b69e30b94164320d
Nico Vogel <nico-vogel97@outlook.de>
2020-06-06 15:56:35 +0000

    refactor

--- lan/event/src/controller/event.controller.ts
+++ lan/event/src/controller/event.controller.ts
@@ -3,25 +3,32 @@ import * as dbService from '../services'
 import * as db from '../models';
 import { eventAdapter } from '../services';
 
+const tip = <T>(callback: (d: T) => void) => (data: T): T => {
+    callback(data);
+    return data;
+}
+
+const notFound = (res: Response) => (data: any) =>
+    data !== undefined ? res.json(data) : res.sendStatus(404);
 
 export function create(req: Request, res: Response, next: NextFunction) {
     dbService.create(db.default.Event, req.body)
-        .then(data => eventAdapter.publish('event.created', { _id: data.id, name: data.title }))
-        .then(res.json)
+        // .then(data => tip(() => eventAdapter.publish('event.created', { _id: data.id, name: data.title }), data))
+        .then(tip((data) => eventAdapter.publish('event.created', { _id: data.id, name: data.title })))
+        .then(data => res.json(data))
         .catch(next)
 }
 
-const notFound = (res: Response, data: any) =>
-    data !== undefined ? res.json(data) : res.sendStatus(404)
+
 
 export function getById(req: Request, res: Response, next: NextFunction) {
     dbService.getById(db.default.Event, req.params.id)
-        .then(event => notFound(res, event))
+        .then(notFound(res))
         .catch(next);
 }
 
 export function getAll(req: Request, res: Response, next: NextFunction) {
     dbService.getAll(db.default.Event)
-        .then(res.json)
+        .then(data => res.json(data))
         .catch(next);
 }
