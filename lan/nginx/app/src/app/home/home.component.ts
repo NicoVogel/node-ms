07f23bd3d9815d0ae0b2ae6701a89cd2cab85e22
Nguyen Viet Do <vietdonguyen@hotmail.de>
2020-06-06 17:13:18 +0000

    style home

--- lan/nginx/app/src/app/home/home.component.ts
+++ lan/nginx/app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  userName;
+  constructor(private authenticationService: AuthenticationService) {
+    this.userName = authenticationService.currentUserValue.name;
+  }
 
   ngOnInit(): void {
   }
