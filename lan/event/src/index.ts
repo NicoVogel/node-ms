90da7c2249a24e7e2e6c6d70dbb12a8c93c908b2
Nico Vogel <nico-vogel97@outlook.de>
2020-06-06 15:00:56 +0000

    implement event router

--- lan/event/src/index.ts
+++ lan/event/src/index.ts
@@ -1,18 +1,19 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
-import { eventAdapter } from './services';
+import { eventAdapter, initAMQP } from './services';
 import * as promBundle from 'express-prom-bundle'
+import EventRouter from './routes/event.routes';
 const app = express();
 
 app.use(promBundle({ includeMethod: true, includePath: true }));
 
-
-
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+
+initAMQP();
+app.use('/', EventRouter);
 
 app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
