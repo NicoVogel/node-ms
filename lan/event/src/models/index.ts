771d833a78e20b7d4520d70120709bd60ac9cb8a
Nico Vogel <nico-vogel97@outlook.de>
2020-06-06 14:57:31 +0000

    export event as defualt schema

--- lan/event/src/models/index.ts
+++ lan/event/src/models/index.ts
@@ -1,11 +1,12 @@
 import * as mongoose from 'mongoose';
 import { dbUrl } from '../config/db.config'
+import Event from './event.model';
 mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
 mongoose.connection.once('open', () => {
   console.log('Service Event connected to database');
 })
 
 const db = {
-
+  Event
 }
 export default db;
