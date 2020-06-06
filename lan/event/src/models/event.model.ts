de1a03fb118a7eb6103d24afecd65fa036695903
Nico Vogel <nico-vogel97@outlook.de>
2020-06-06 14:46:43 +0000

    create event model

--- lan/event/src/models/event.model.ts
+++ lan/event/src/models/event.model.ts
@@ -2,8 +2,19 @@ import * as mongo from 'mongoose';
 import { JSONOptions } from '../config/db.config';
 
 export interface IEvent extends mongo.Document {
-    _id: number;
+    _id: object;
     title: string;
     created: Date;
-    
+    description: string;
 }
+
+export const eventSchema = new mongo.Schema({
+    created: { type: Date, default: Date.now },
+    title: String,
+    description: String
+})
+
+eventSchema.set('toJSON', JSONOptions);
+
+const Event = mongo.model<IEvent>('Event', eventSchema);
+export default Event;
