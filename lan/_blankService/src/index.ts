8ec5254f15ecd6d5cef7d05d8d2ec709ed98cec7
Nico Vogel <nico-vogel97@outlook.de>
2020-06-06 13:10:17 +0000

    add promotheus middle ware in all servies

--- lan/_blankService/src/index.ts
+++ lan/_blankService/src/index.ts
@@ -1,7 +1,10 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
+import * as promBundle from 'express-prom-bundle'
 const app = express();
+
+app.use(promBundle({ includeMethod: true, includePath: true }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
--- lan/account/src/index.ts
+++ lan/account/src/index.ts
@@ -5,8 +5,10 @@ import UserRouter from './routes/user.routes'
 import TeamRouter from './routes/team.routes'
 import { jwt } from './services/jwt.service'
 import { initAMQP } from './services';
-
+import * as promBundle from 'express-prom-bundle'
 const app = express();
+
+app.use(promBundle({ includeMethod: true, includePath: true }));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
